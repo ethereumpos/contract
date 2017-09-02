7cafc2f58593a1c7e4e7b108dbf48b5556f2442e
Hunter Long <Info@socialeck.com>
2017-09-02 00:31:09 -0600

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -7,15 +7,68 @@ contract EthereumPOS {
     mapping(address => bool) activeSubscription;
     mapping(int256 => string) startDate;
     mapping(int256 => int) startBlock;
+    address public approvedSender;
+    address private creator;
     
-    function subscriptionActive(int256 id) constant returns (bool);
-    function subscriptionStartedDate(int256 id) constant returns (string);
-    function subscriptionStartedBlock(int256 id) constant returns (int);
-    function subscriptionExpiresDate(int256 id) constant returns (string);
-    function subscriptionExpiresBlock(int256 id) constant returns (int);
-    function subscriptionAddressActive(address wallet) constant returns (bool);
-    function orderPaid(int256 id) constant returns (bool);
-    function orderPaidBlock(int256 id) constant returns (int);
-    function approvedSender() constant returns (address);
+    struct Subscription {
+        int256 orderId;
+        string redId;
+        address refAddress;
+        bool active;
+        int256 expiresBlock;
+    }
+    
+    function EthereumPOS() {
+        creator = msg.sender;
+    }
+    
+    function changeCreator(address _new){
+        require(msg.sender==creator);
+        creator = _new;
+    }
+    
+    function changeSender(address _new){
+        require(msg.sender==creator);
+        approvedSender = _new;
+    }
+    
+    
+    function subscriptionActive(int256 id) constant returns (bool) {
+        
+    }
+    
+    function subscriptionStartedDate(int256 id) constant returns (string){
+        
+    }
+    
+    function subscriptionStartedBlock(int256 id) constant returns (int){
+        
+    }
+    
+    function subscriptionExpiresDate(int256 id) constant returns (string){
+        
+    }
+    
+    function subscriptionExpiresBlock(int256 id) constant returns (int){
+        
+    }
+    
+    function subscriptionAddressActive(address wallet) constant returns (bool){
+        
+    }
+    
+    function orderPaid(int256 id) constant returns (bool){
+        
+    }
+    
+    function orderPaidBlock(int256 id) constant returns (int){
+        
+    }
+    
+    function approvedSender() constant returns (address){
+        
+    }
+    
+    
     
 }
